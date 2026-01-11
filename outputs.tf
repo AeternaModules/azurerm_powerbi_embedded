0345ba33eb637e24133443c1be853f98cc5dca36
Daniel Allen <dan@aeternagroup.net>
2026-01-11 17:54:01 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "powerbi_embeddeds" {
+  description = "All powerbi_embedded resources"
+  value       = azurerm_powerbi_embedded.powerbi_embeddeds
+}
+
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "powerbi_embeddeds" {
+  description = <<EOT
+Map of powerbi_embeddeds, attributes below
+Required:
+    - administrators
+    - location
+    - name
+    - resource_group_name
+    - sku_name
+Optional:
+    - mode
+    - tags
+EOT
+
+  type = map(object({
+    administrators      = set(string)
+    location            = string
+    name                = string
+    resource_group_name = string
+    sku_name            = string
+    mode                = optional(string, "Gen1")
+    tags                = optional(map(string))
+  }))
+}
+
